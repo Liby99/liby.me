91fe9df2b274e6893de40f15a345746d3dd85ef1
Liby99 <liby99@icloud.com>
2016-09-27 10:47:08 -0700

    Done Message Display

--- src/server/api/database.js
+++ src/server/api/database.js
@@ -2,6 +2,8 @@
  *
  */
 
+var mysql = require("../module/mysql.js");
+
 module.exports = {
     /**
      * [get_top_10_artworks description]
@@ -13,5 +15,29 @@ module.exports = {
     },
     getArtworkOfYear: function (year, callback) {
         
+    },
+    getMessages: function (callback) {
+        mysql.query("SELECT `username`, `email`, `date_time`, `content`  FROM `message` ORDER BY `date_time` DESC", {}, function (err, result) {
+            if (err) {
+                callback(false);
+            }
+            else {
+                callback(result);
+            }
+        });
+    },
+    addMessage: function (username, email, content, callback) {
+        mysql.query("INSERT INTO `message` SET `date_time` = NOW(), `read` = 0, ?", {
+            "username": username,
+            "email": email,
+            "content": content
+        }, function (err, result) {
+            if (err) {
+                callback(false);
+            }
+            else {
+                callback(true);
+            }
+        });
     }
 }
--- src/server/route/admin/message.js
+++ src/server/route/admin/message.js
@@ -1,7 +1,12 @@
 var admin = require("../../api/admin.js");
+var database = require("../../api/database.js");
 
 module.exports = function (req, res) {
     admin.verify(req, res, function () {
-        res.render("admin/message");
+        database.getMessages(function (messages) {
+            res.render("admin/message", {
+                "messages": messages
+            });
+        });
     });
 }
