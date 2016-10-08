26461d28486e865d1e82a4dfc1c9c0ac9931eb07
Liby99 <liby99@icloud.com>
2016-10-07 18:28:00 -0700

    Fixed bug of navbar

--- src/public/js/basic.js
+++ src/public/js/basic.js
@@ -1,6 +1,7 @@
 var Nav = {
     $toggle: $("#nav-menu-toggle"),
     $menu: $("#nav-menu-outer"),
+    $menuMask: $("#nav-menu-mask"),
     initiate: function () {
         this.initiateNav();
         this.initiateNavToggle();
@@ -9,11 +10,11 @@ var Nav = {
     initiateNavToggle: function () {
         var self = this;
         self.$toggle.click(function () {
-            if (self.$menu.hasClass("active")) {
-                self.$menu.removeClass("active");
+            if (self.menuShown()) {
+                self.hideMenu();
             }
             else {
-                self.$menu.addClass("active");
+                self.showMenu();
             }
         });
     },
@@ -25,5 +26,16 @@ var Nav = {
             var name = PAGE.substring(0, 1).toUpperCase() + PAGE.substring(1);
             $("title").html($("title").html() + " - " + name);
         }
+    },
+    menuShown: function () {
+        return this.$menu.hasClass("active");
+    },
+    showMenu: function () {
+        this.$menu.addClass("active");
+        this.$menuMask.fadeIn("fast");
+    },
+    hideMenu: function () {
+        this.$menu.removeClass("active");
+        this.$menuMask.fadeOut("fast");
     }
 }
