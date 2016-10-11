e2fe85d75c72cbf8e141082867bc6e42bf818dc9
Liby99 <liby99@icloud.com>
2016-10-11 10:32:41 -0700

    Done with homepage

--- src/server/route/index.js
+++ src/server/route/index.js
@@ -1,10 +1,19 @@
 var Artwork = require("../api/artwork.js");
+var Article = require("../api/article.js");
 
 module.exports = function (req, res, callback) {
     Artwork.getLatestArtworks(function (artworks) {
         if (artworks) {
-            callback({
-                "artworks": artworks
+            Article.getLatestArticles(function (articles) {
+                if (articles) {
+                    callback({
+                        "artworks": artworks,
+                        "articles": articles
+                    });
+                }
+                else {
+                    res.redirect("error.html?err=500");
+                }
             });
         }
         else {
