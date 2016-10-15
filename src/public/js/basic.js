5f549ab5960dcfea24fd0771e71d2008369d050d
Liby99 <liby99@icloud.com>
2016-10-14 23:15:33 -0700

    Updated encapsulated ajax function

--- src/public/js/basic.js
+++ src/public/js/basic.js
@@ -43,3 +43,31 @@ var Nav = {
         this.$menuMask.fadeOut("fast");
     }
 }
+
+function ajax(options) {
+    $.ajax({
+        url: options["url"],
+        type: options["type"],
+        data: options["data"],
+        success: function (result) {
+            var data = JSON.parse(result);
+            if (data["error_code"] == 0) {
+                options["success"](data["content"]);
+            }
+            else if (options["error"]) {
+                options["error"](data["error_code"], data["error_log"], data["content"]);
+            }
+            else {
+                alert("Error " + data["error_code"] + ": " + data["error_log"]);
+            }
+        },
+        error: function () {
+            if (options["error"]) {
+                options["error"](0, "Server connection failed");
+            }
+            else {
+                alert("Error: Server connection failed");
+            }
+        }
+    })
+}
