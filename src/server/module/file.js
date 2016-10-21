c31f6e5da38a2202dd48fe1f7b2de17c5580fa79
Liby99 <liby99@icloud.com>
2016-10-20 21:34:13 -0700

    Add decoding for file saving

--- src/server/module/file.js
+++ src/server/module/file.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 
 module.exports = {
     saveImage: function (name, data, callback) {
-        fs.writeFile("../../public/img/" + name, data, function (err) {
+        fs.writeFile("../../public/img/" + name, decodeBase64Image(data);, function (err) {
             if (err) {
                 callback(false);
             }
@@ -11,4 +11,12 @@ module.exports = {
             }
         });
     }
+}
+
+function decodeBase64Image(data) {
+    var matches = data.match(regex);
+    if (matches.length !== 3) {
+        throw new Error('Invalid input string');
+    }
+    return new Buffer(matches[2], 'base64');
 }
